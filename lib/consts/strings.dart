cac2d72035356bdbfc3df61d70cc3efeda364b38
ZeeGalaxy007 <107889097+ZeeGalaxy007@users.noreply.github.com>
2022-12-27 19:22:05 +0530

    first commit

---
+++ lib/consts/strings.dart
@@ -0,0 +1,6 @@
+const String degree = "°";
+const String apiKey = "1c62eb57f5fcbd4135c623a8788ff7b9";
+const String currentEndpoint =
+    "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}";
+const String hourlyEndpoint =
+    "api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}";
